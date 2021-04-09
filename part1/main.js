b40e93d192b800765b4903034ba211a5e6570361
kaitoohsiro <kaitooshiro7@gmail.com>
2021-04-09 15:02:16 +0900

    first commit

---
+++ part1/main.js
@@ -0,0 +1,6 @@
+"use strict";
+function getTotalPrice(price, unit) {
+    return price * unit;
+}
+console.log(getTotalPrice(100, 10));
+console.log(getTotalPrice(100, 10));
---
+++ part1/main.ts
@@ -0,0 +1,6 @@
+function getTotalPrice(price: number, unit: number) {
+    return price * unit;
+}
+console.log(getTotalPrice(100, 10));
+console.log(getTotalPrice(100, 10));
+
